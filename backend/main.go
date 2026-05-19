3d4a7d9992cd2014d37e1e23d053b40b3ce74d4e
AnandaDly <rizkyanandaalamsyahdly@gmail.com>
2026-05-19 11:41:36 +0700

    feat: setup gorm connection and auto-migrate schemas

--- backend/main.go
+++ backend/main.go
@@ -5,8 +5,25 @@ import (
 	"log"
 	"os"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
 	"github.com/joho/godotenv"
 )
+
+type Event struct {
+	ID               uint
+	Name             string
+	TotalTickets     int
+	AvailableTickets int
+}
+
+type TicketOrder struct {
+	ID      uint
+	EventID uint
+	UserID  string
+	Status  string
+}
 
 func main() {
 	fmt.Println("Memulai Ticket War Engine...")
@@ -18,6 +35,15 @@ func main() {
 
 	DB_URL := os.Getenv("DB_URL")
 	fmt.Println(DB_URL)
+
+	db, err := gorm.Open(postgres.Open(DB_URL), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	} else {
+		fmt.Println("Berhasil Connect ke database")
+	}
+
+	db.AutoMigrate(&Event{}, &TicketOrder{})
 	// REDIS_URL := os.Getenv("REDIS_URL")
 
 }
